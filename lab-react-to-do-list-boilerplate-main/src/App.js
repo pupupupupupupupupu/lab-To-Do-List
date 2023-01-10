3e2a5627ac5691ed65edcb9f6568e2f5e731fed1
Arsalan Hassan <114077170+pupupupupupupupupu@users.noreply.github.com>
2023-01-10 14:42:34 +0530

    Add files via upload

---
+++ lab-react-to-do-list-boilerplate-main/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ToDoApp from "./components/ToDoApp";
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoApp></ToDoApp>
+    </div>
+  );
+}
+
+export default App;
